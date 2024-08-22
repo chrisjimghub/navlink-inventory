f98a1a995cacb760f07708b7246ceead566a4e6f
chris <jimegot@yahoo.com.ph>
2024-08-22 08:42:32 +0800

    add ons permission manager and filter

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; 
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use CrudTrait; 
+    use HasRoles; 
 
     /**
      * The attributes that are mass assignable.
